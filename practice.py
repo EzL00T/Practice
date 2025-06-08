39c29891f7974b3ea1bd1ad7a240bff8d6ef13de
Yurii <yuriikroitor@gmail.com>
2025-06-08 16:32:44 +0300

    +dict

--- practice.py
+++ practice.py
@@ -1,16 +1,18 @@
 week_limit = float(input('Витрати на неділю: '))
 
-monday = float(input("Витрати на пн: "))
-tuesday = float(input("Витрати на вт: "))
-wednesday = float(input("Витрати на ср: "))
-thursday = float(input("Витрати на чт: "))
-friday = float(input("Витрати на пт: "))
-saturday = float(input("Витрати на сб: "))
-sunday = float(input("Витрати на нд: "))
+days = ["пн", "вт", "ср", "чт", "пт", "сб", "нд"]
+expenses = {}
 
-all_day = monday + tuesday + wednesday + thursday + friday + saturday + sunday
+for day in days:
+    expenses[day] = float(input(f"Витрати на {day}: "))
+
+all_day = sum(expenses.values())
 
 if all_day < week_limit:
     print(f'Залишок: {week_limit - all_day}')
 else:
     print('Перевищено ліміт')
+
+print("\nВитрати по днях:")
+for day in days:
+    print(f"{day}: {expenses[day]}")
